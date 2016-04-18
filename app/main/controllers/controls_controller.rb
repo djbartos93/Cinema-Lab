308486bff88b55aa74c308eb72effdb081332248
Eric Bartos <djbartos93@gmail.com>
2016-04-18 10:46:07 -0400

    adding some functionality

--- app/main/controllers/controls_controller.rb
+++ app/main/controllers/controls_controller.rb
@@ -1,5 +1,19 @@
 module Main
   class ControlsController < Volt::ModelController
 
+    def BTN_poweron
+        puts "power on"
+    end
+
+    def BTN_poweroff
+      puts "power off"
+    end
+    def power_on
+      
+    end
+
+    def power_off
+
+    end
   end
 end
