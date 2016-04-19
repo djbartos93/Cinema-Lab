54b5b35d6f27ee783f8ae698737953980be3b147
Eric Bartos <djbartos93@gmail.com>
2016-04-19 02:56:19 -0400

    different gem

--- app/main/tasks/control_task.rb
+++ app/main/tasks/control_task.rb
@@ -2,6 +2,11 @@ class ControlTask < Volt::Task
 
 Delay_Time = 2
 
+io = WiringPi::GPIO.new do |gpio|
+  gpio.pin_mode(0, WiringPi::OUTPUT)
+end
+
+
   def power(status)
     if status == "on"
       puts "the power is now on"
@@ -12,35 +17,25 @@ Delay_Time = 2
     end
   end
 def delay_on
-  setup
-  circuit1.on
+  io.digital_write(0, WiringPi::HIGH)
   puts "circuit 1 ON"
   sleep(Delay_Time)
-  circuit2.on
   puts "circuit 2 ON"
   sleep(Delay_Time)
-  circuit3.on
   puts "circuit 3 ON"
   sleep(Delay_Time)
-  circuit4.on
   puts "circuit 4 ON"
   sleep(Delay_Time)
-  circuit5.on
   puts "circuit 5 ON"
   sleep(Delay_Time)
-  circuit6.on
   puts "circuit 6 ON"
   sleep(Delay_Time)
-  circuit7.on
   puts "circuit 7 ON"
   sleep(Delay_Time)
-  circuit8.on
   puts "circuit 8 ON"
   sleep(Delay_Time)
-  circuit9.on
   puts "circuit 9 ON"
   sleep(1)
-  circuit10.on
   puts "System ON"
 end
 
@@ -52,18 +47,7 @@ def dlay_off
   puts ""
 end
 
-def setup
-  circuit1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit2 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit3 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit4 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit5 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit6 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit7 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit8 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit9 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-  circuit10 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-end
+
 
 
 end
